4744bc3d20f1a5f0e3448652b96e32f45a6e119b
Hugo-LL1 <hleloutre@fi.uba.ar>
2026-09-13 10:41:03 -0300

    Insert query y agrego postman al .gitignore

--- app.py
+++ app.py
@@ -35,9 +35,22 @@ def crear_socio():
     if not re.match(patron_email, email):
         return {"error": "El formato de email ingresado no es valido"}, 400
 
-    
-
-
+    conexion = obtener_conexion()
+    cursor = conexion.cursor(dictionary=True)
+    consulta = "SELECT * FROM socios WHERE email = %s"
+    cursor.execute(consulta, (email,))
+    socio_existente = cursor.fetchone()
+    if socio_existente:
+        cursor.close()
+        conexion.close()
+        return {"error" : "El email ya se encuentra registrado"}, 409
+    else:
+        insert_query = "INSERT INTO socios (nombre, email, activo) VALUES (%s, %s, %s)"
+        cursor.execute(insert_query, (nombre, email, True))
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+        return {"mensaje": "El socio fue creado con exito"}, 201
         
 
 
