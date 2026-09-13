6e47408b5a6d5f6e73f8dc7259706412661d03b2
Hugo-LL1 <hleloutre@fi.uba.ar>
2026-09-13 16:47:28 -0300

    Implementacion de la libreria jsonify y estandarizacion de uso de app.route

--- app.py
+++ app.py
@@ -21,19 +21,19 @@ def obtener_conexion():
 
     return conexion
 
-@app.post("/socios")
+@app.route('/socios', methods=['POST'])
 def crear_socio():
     data = request.get_json()
     nombre = data.get('nombre')
     email = data.get('email')
     if not nombre:
-        return {"error": "El nombre es obligatorio"}, 400
+        return jsonify({"error": "El nombre es obligatorio"}), 400
     if not email:
-        return {"error": "El email es obligatorio"}, 400
+        return jsonify({"error": "El email es obligatorio"}), 400
     email = email.strip().lower()
     patron_email = r'^[\w\.-]+@[\w\.-]+\.\w+$'
     if not re.match(patron_email, email):
-        return {"error": "El formato de email ingresado no es valido"}, 400
+        return jsonify({"error": "El formato de email ingresado no es valido"}), 400
 
     conexion = obtener_conexion()
     cursor = conexion.cursor(dictionary=True)
@@ -43,14 +43,14 @@ def crear_socio():
     if socio_existente:
         cursor.close()
         conexion.close()
-        return {"error" : "El email ya se encuentra registrado"}, 409
+        return jsonify({"error" : "El email ya se encuentra registrado"}), 409
     else:
         insert_query = "INSERT INTO socios (nombre, email, activo) VALUES (%s, %s, %s)"
         cursor.execute(insert_query, (nombre, email, True))
         conexion.commit()
         cursor.close()
         conexion.close()
-        return {"mensaje": "El socio fue creado con exito"}, 201
+        return jsonify({"mensaje": "El socio fue creado con exito"}), 201
         
 
 
