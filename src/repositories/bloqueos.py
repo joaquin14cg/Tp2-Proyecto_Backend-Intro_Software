5637bd19bf4315e45ef763a601ab641e8342755b
Your Name <sandrejin@fi.uba.ar>
2026-09-19 19:54:31 -0300

    modificacion de bloqueos.py para que tenga conexion

--- src/repositories/bloqueos.py
+++ src/repositories/bloqueos.py
@@ -1,6 +1,19 @@
 from flask import Blueprint, request, jsonify
-from src.models.bloqueos import Bloqueo
+from db import obtener_conexion
+
 
 def obtener_bloqueos():
-    bloqueos = Bloqueo.query.all()
+    conexion = obtener_conexion()
+    cursor = conexion.cursor(dictionary=True) #El cursor es el objeto que utilizás para ejecutar SQL.
+
+    cursor.execute("""
+        SELECT id, cancha_id, fecha_bloqueo, inicio, fin, motivo
+        FROM bloqueos
+    """)
+
+    bloqueos = cursor.fetchall()
+
+    cursor.close()
+    conexion.close()
+
     return bloqueos
